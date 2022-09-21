af4c1836125e2f2b604e1bafed18cb0109003cdc
khido <mcdonald.am97@gmail.com>
2022-09-21 01:02:11 -0400

    add field job title

--- modules/aad-user/variables.tf
+++ modules/aad-user/variables.tf
@@ -12,3 +12,10 @@ variable "password" {
   type        = string
   description = "Temporary password"
 }
+
+variable "job_title" {
+  type        = string
+  description = "Temporary password"
+  default     = "Cloud Administrator"
+
+}
