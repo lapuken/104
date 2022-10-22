5766550f219f0e8dd93d65150b12d89241c7eb62
khido <mcdonald.am97@gmail.com>
2022-10-22 17:18:20 -0400

    Review things

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ terraform {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tamopstfkhido"
-    container_name       = "tfstatedevops"
+    #resource_group_name  = "tamopstfstates"
+    #storage_account_name = "tamopstfkhido"
+    #container_name       = "tfstatedevops"
     #key                  = "terraformgithubexample.tfstate"
   }
 }
